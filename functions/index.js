6b85a87222796601cf6b110e98620aa58d41bb9e
Federico Tibaldo <fedetibaldo@protonmail.com>
2020-11-14 01:30:31 +0100

    Search and retweet function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const Twitter = require('twitter');
+const auth = require('./twitter-auth.json');
+
+exports.searchAndRetweet = functions.pubsub.schedule('every 15 minutes').onRun(searchAndRetweet);
+
+async function searchAndRetweet() {
+	const client = new Twitter(auth);
+	let tweets = await client.get('statuses/home_timeline', { tweet_mode: 'extended' });
+	tweets = tweets.filter(tweet => tweet.entities.hashtags.some(hashtag => hashtag.text.toLowerCase() == 'decadejam'));
+	tweets = tweets.filter(tweet => !tweet.retweeted);
+	tweets.forEach(tweet => client.post(`statuses/retweet/${tweet.id_str}`, {}));
+}
