b7060cdbc7bd426e5f74f8aee5a2eebdf8bbf153
Federico Tibaldo <fedetibaldo@protonmail.com>
2020-11-14 13:32:38 +0100

    Search among all tweets

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,14 @@ exports.searchAndRetweet = functions.pubsub.schedule('every 15 minutes').onRun(s
 
 async function searchAndRetweet() {
 	const client = new Twitter(auth);
-	let tweets = await client.get('statuses/home_timeline', { tweet_mode: 'extended' });
-	tweets = tweets.filter(tweet => tweet.entities.hashtags.some(hashtag => hashtag.text.toLowerCase() == 'decadejam'));
-	tweets = tweets.filter(tweet => !tweet.retweeted);
-	tweets.forEach(tweet => client.post(`statuses/retweet/${tweet.id_str}`, {}));
+	let tweets = await client.get('search/tweets', { q: '#decadejam', result_type: 'recent', tweet_mode: 'extended' });
+	tweets = tweets.statuses.filter(tweet => tweet.entities.hashtags.some(hashtag => hashtag.text.toLowerCase() == 'decadejam'));
+	tweets = tweets.filter(tweet => !tweet.retweeted_status || tweet.is_quote_status);
+	tweets.forEach(async (tweet) => {
+		try {
+			await client.post(`statuses/retweet/${tweet.id_str}`, {})
+		} catch (error) {
+			// code: 327, message: 'You have already retweeted this Tweet.'
+		}
+	});
 }
